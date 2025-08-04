b21fff87464476426a274ac4eafe2889b5a9bb8a
josgomz <josgomlir@gmail.com>
2025-08-04 04:46:03 -0600

    Configure project for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "static",
+  site: "https://josgomz.github.io", // Tu URL de GitHub Pages
+  base: "/CV-Bitacora", // El nombre de tu repositorio
 });
